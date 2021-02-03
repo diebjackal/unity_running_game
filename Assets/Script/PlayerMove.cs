b329641413083b70d21c9223c323432a278b50e3
diebjackal <55783877+diebjackal@users.noreply.github.com>
2021-02-03 20:28:58 +0900

    running game

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal")){
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        //Dirction Sprite
+        if(Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        //Walking Animation, Abs == 절대값
+        if(Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("IsWalk", false);
+        else
+            anim.SetBool("IsWalk", true);
+
+    }
+    private void FixedUpdate()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //Max Speed
+        if(rigid.velocity.x > maxSpeed) // right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+    }
+}
