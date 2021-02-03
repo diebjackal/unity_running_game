3b8b98b865544aa210eb3ec83c3018343cb69483
diebjackal <55783877+diebjackal@users.noreply.github.com>
2021-02-04 00:31:28 +0900

    [Add] Ai
    Make to Enemy A.I

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
 
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
-    Animator anim;
+    Animator anim;  
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -17,9 +18,16 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update()
     {
+        //Player Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("IsJump", true);
+        }
+        
         //Stop Speed
         if (Input.GetButtonUp("Horizontal")){
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f, rigid.velocity.y);
         }
         //Dirction Sprite
         if(Input.GetButton("Horizontal"))
@@ -44,5 +52,17 @@ public class PlayerMove : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
 
+        //Landing Platform
+        if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("IsJump", false);
+            }
+        }
     }
 }
